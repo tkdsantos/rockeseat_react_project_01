5482f03e6d5ebf3f9c12862b010b84a6792b6728
daniel <tkdsantos@gmail.com>
2023-09-11 21:26:42 -0300

    Componente Avatar

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,3 +1,4 @@
+import { Avatar } from "./Avatar"
 import { Comment } from "./Comment"
 import styles from "./Post.module.css"
 
@@ -6,7 +7,7 @@ export function Post() {
         <article className={styles.post}>
             <header>
                 <div className={styles.author}>
-                    <img src="https://github.com/tkdsantos.png" />
+                    <Avatar hasBorder src="https://github.com/tkdsantos.png" />
                     <div className={styles.authorInfo}>
                         <strong>Daniel Santos</strong>
                         <span>Web Developer</span>
@@ -33,7 +34,6 @@ export function Post() {
                 <Comment />
                 <Comment />
                 <Comment />
-
             </div>
 
         </article>
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,3 +1,4 @@
+import { Avatar } from './Avatar'
 import styles from './Sidebar.module.css'
 
 import {PencilLine} from "phosphor-react"
@@ -7,10 +8,10 @@ export function Sidebar () {
         <aside className={styles.sidebar}>
             <img className={styles.cover} src="https://images.unsplash.com/photo-1605379399642-870262d3d051?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=600&q=60" />
             <div className={styles.profile}>
-                <img className={styles.avatar} src="https://github.com/tkdsantos.png" />
+                <Avatar src="https://github.com/tkdsantos.png" />
                 <strong>Daniel Santos</strong>
                 <span>Web Developer</span>
-            </div>
+            </div> 
             <footer>
                 <a href="#">
                     < PencilLine size={20}/>
